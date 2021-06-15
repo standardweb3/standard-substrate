2987641b45a6375faef1e0bdf05a069f1cd25a3d
firke <edvards.sedols@gmail.com>
2021-06-15 17:05:10 +0100

    Firke/dockerfile executable set (#12)
    * Docker entrypoint script changed, init.sh updated to use nightly build, Makefile additional command.

--- Docker/Docker.md
+++ Docker/Docker.md
@@ -1,1 +1,3 @@
 # Docker instructions
+
+Requires BuildKit.
--- scripts/init.sh
+++ scripts/init.sh
@@ -5,8 +5,10 @@ set -e
 echo "*** Initializing WASM build environment"
 
 if [ -z $CI_PROJECT_NAME ] ; then
-   rustup update nightly
-   rustup update stable
+   rustup install nightly-2021-03-01
+   rustup default nightly-2021-03-01
+   rustup update nightly-2021-03-01
+   rustup target add wasm32-unknown-unknown --toolchain nightly-2021-03-01
 fi
 
 rustup target add wasm32-unknown-unknown --toolchain nightly
