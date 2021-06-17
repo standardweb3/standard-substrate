78e0071b583893eafbe6267fc70176e8ec078773
firke <edvards.sedols@gmail.com>
2021-06-17 12:13:29 +0100

    Fix for entrypoint script.

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,11 +1,10 @@
-#!/bin/sh
-set -e
+#!/bin/bash
+set -eo pipefail
 
 echo "print dependencies"
-exec ldd /usr/local/bin/opportunity-standalone
+ldd /usr/local/bin/opportunity-standalone
 echo "get binary version"
-exec /usr/local/bin/opportunity-standalone --version
-# echo "wait for efs"
-# exec sleep 30
+/usr/local/bin/opportunity-standalone --version
 echo "startup node"
-exec /usr/local/bin/opportunity-standalone
+# exec /usr/local/bin/opportunity-standalone
+exec "$@"
