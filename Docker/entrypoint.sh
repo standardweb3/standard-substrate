725f44c30ca67b2b461721a7149c76edeccfc246
firke <edvards.sedols@gmail.com>
2021-06-17 12:25:59 +0100

    Fix for entrypoint script.

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -6,5 +6,4 @@ ldd /usr/local/bin/opportunity-standalone
 echo "get binary version"
 /usr/local/bin/opportunity-standalone --version
 echo "startup node"
-# exec /usr/local/bin/opportunity-standalone
 exec "$@"
