a54059ee6ec7a52c36e0cde2375470d565b90f38
firke <edvards.sedols@gmail.com>
2021-06-18 12:51:18 +0100

    Updates to oneline-upgrade script.

--- scripts/oneline-upgrade.sh
+++ scripts/oneline-upgrade.sh
@@ -1,32 +1,42 @@
 #!/bin/bash
 set -e
 
+$FILE=/usr/local/bin/opportunity-standalone
 if [[ $(whoami) == "root" ]]; then
     MAKE_ME_ROOT=
+    echo "root already, continuing"
 else
     MAKE_ME_ROOT=sudo
+    echo "will use sudo"
 fi
 
-# stops the process
+
+echo "stopping systemd service"
 $MAKE_ME_ROOT systemctl stop standard-validator
 
 # moves binary to be a backup
-$MAKE_ME_ROOT mv /usr/local/bin/opportunity-standalone /usr/local/bin/opportunity-standalone-backup
+if [ -f "$FILE" ]; then
+  echo "$FILE exists, moving"
+  $MAKE_ME_ROOT mv $FILE $FILE-backup
+else 
+    echo "$FILE does not exist."
+fi
 
-# pulls latest release tag and sets it as var
-$MAKE_ME_ROOT LATEST_RELEASE=`curl --silent "https://api.github.com/repos/digitalnativeinc/standard-substrate/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'`
+echo "pulling latest release tag and setting it as var"
+LATEST_RELEASE=`curl --silent "https://api.github.com/repos/digitalnativeinc/standard-substrate/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'`
+echo "$LATEST_RELEASE"
 
-# downloads binary based on latest tag set
-$MAKE_ME_ROOT wget -O /usr/local/bin/opportunity-standalone https://github.com/digitalnativeinc/standard-substrate/releases/download/$LATEST_RELEASE/opportunity-standalone-linux-x86_64
+echo "downloading binary based on latest tag set"
+$MAKE_ME_ROOT wget -O $FILE https://github.com/digitalnativeinc/standard-substrate/releases/download/$LATEST_RELEASE/opportunity-standalone-linux-x86_64
 
-# make binary executable
-$MAKE_ME_ROOT chmod +x /usr/local/bin/opportunity-standalone
+echo "making binary executable"
+$MAKE_ME_ROOT chmod +x $FILE
 
-# restart the service
+echo "starting up the service"
 $MAKE_ME_ROOT systemctl start standard-validator
 
-# check it started up correctly
+echo "checking if service started up correctly"
 $MAKE_ME_ROOT systemctl status standard-validator
 
-# check logs to see if there are any issues
-$MAKE_ME_ROOT journalctl -u standard-validator -f
+echo "checking logs to see if there are any issues"
+$MAKE_ME_ROOT journalctl -u standard-validator
