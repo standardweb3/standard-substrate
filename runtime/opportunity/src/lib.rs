8753151518cff24fe62f964cc394b7c807e4944e
firke <edvards.sedols@gmail.com>
2021-07-13 17:43:45 +0100

    Switch back RuntimeVersion.

--- runtime/opportunity/src/lib.rs
+++ runtime/opportunity/src/lib.rs
@@ -115,26 +115,11 @@ pub fn wasm_binary_unwrap() -> &'static [u8] {
     )
 }
 
-// To learn more about runtime versioning and what each of the following value means:
-//   https://substrate.dev/docs/en/knowledgebase/runtime/upgrades#runtime-versioning
-#[sp_version::runtime_version]
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-<<<<<<< HEAD
     spec_name: create_runtime_str!("opportunity"),
     impl_name: create_runtime_str!("opportunity10"),
     authoring_version: 1,
     spec_version: 10,
-=======
-    spec_name: create_runtime_str!("node-template"),
-    impl_name: create_runtime_str!("node-template"),
-    authoring_version: 1,
-    // The version of the runtime specification. A full node will not attempt to use its native
-    //   runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
-    //   `spec_version`, and `authoring_version` are the same between Wasm and native.
-    // This value is set to 100 to notify Polkadot-JS App (https://polkadot.js.org/apps) to use
-    //   the compatible custom types.
-    spec_version: 100,
->>>>>>> 3893f18 (Cargo fmt run on codebase.)
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
