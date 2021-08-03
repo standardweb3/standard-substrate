e5cb133793a78e8b757fb58cf04d8ac3914379bb
Trans-Siberian Railway <tsrailway@protonmail.com>
2021-08-03 17:05:16 +0100

    Update Cargo version to match the one we aim for - 0.9.7, increment spec_version in runtime.

--- runtime/opportunity/src/lib.rs
+++ runtime/opportunity/src/lib.rs
@@ -131,7 +131,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("opportunity"),
 	impl_name: create_runtime_str!("opportunity10"),
 	authoring_version: 1,
-	spec_version: 20,
+	spec_version: 21,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
