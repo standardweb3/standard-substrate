02b65df39ef269fc658c5d4015906df956756a14
hskang9 <hskang9@github.com>
2021-08-16 23:45:48 +0900

    update operator -> provider [oracle]

--- pallets/oracle/src/lib.rs
+++ pallets/oracle/src/lib.rs
@@ -31,36 +31,36 @@ decl_module! {
 
 
 		// REVIEW: Use `///` instead of `//` to make these doc comments that are part of the crate documentation.
-		// Register a new Operator.
-		// Fails with `OperatorAlreadyRegistered` if this Operator (identified by `origin`) has already been registered.
+		// Register a new Provider.
+		// Fails with `ProviderAlreadyRegistered` if this Provider (identified by `origin`) has already been registered.
 		#[weight = 10_000]
 		pub fn register_operator(origin, _socket: SocketIndex, _who: T::AccountId) -> DispatchResult {
 			ensure_root(origin)?;
 			Providers::<T>::insert(&_who, true);
 			Sockets::<T>::insert(_socket, _who.clone());
-			Self::deposit_event(RawEvent::OperatorRegistered(_who));
+			Oracles::<T>::insert(_who.clone(), _socket);
+			Self::deposit_event(RawEvent::ProviderRegistered(_who));
 
 			Ok(())
 		}
 
-		// Unregisters an existing Operator
+		// Unregisters an existing Provider
 		// TODO check weight
 		#[weight = 10_000]
-		pub fn deregister_operator(origin) -> DispatchResult {
-			let who : <T as frame_system::Config>::AccountId = ensure_signed(origin)?;
-
-			if Providers::<T>::take(who.clone()) {
-				Self::deposit_event(RawEvent::OperatorUnregistered(who));
-				Ok(())
-			} else {
-				Err(Error::<T>::UnknownOperator.into())
-			}
+		pub fn deregister_operator(origin, _socket: SocketIndex, _who: T::AccountId) -> DispatchResult {
+			ensure_root(origin)?;
+			Providers::<T>::remove(&_who);
+			Sockets::<T>::remove(_socket);
+			Oracles::<T>::remove(_who.clone());
+			Self::deposit_event(RawEvent::ProviderDeregistered(_who));
+				
+			Ok(())
 		}
 
 		#[weight = 0]
 		fn report(origin, _socket: SocketIndex, _id: AssetId, _price: Balance) -> DispatchResult {
 			let who : <T as frame_system::Config>::AccountId = ensure_signed(origin)?;
-			ensure!(Providers::<T>::contains_key(who.clone()), Error::<T>::WrongOperator);
+			ensure!(Providers::<T>::contains_key(who.clone()), Error::<T>::WrongProvider);
 			ensure!(Sockets::<T>::get(_socket) == who.clone(), Error::<T>::WrongSocket);
 			let results = match Self::asset_price(_id) {
 				Some(mut x) => {
@@ -169,10 +169,10 @@ decl_event! {
 		<T as frame_system::Config>::AccountId,
 	{
 		// A new operator has been registered
-		OperatorRegistered(AccountId),
+		ProviderRegistered(AccountId),
 
 		// An existing operator has been unregistered
-		OperatorUnregistered(AccountId),
+		ProviderDeregistered(AccountId),
 
 		// Price reported by an oracle provider
 		PriceSubmitted(SocketIndex, AccountId, u128),
@@ -183,25 +183,23 @@ decl_error! {
 	pub enum Error for Module<T: Config>  {
 		/// Error names should be descriptive.
 		NoneValue,
-		/// Errors should have helpful documentation associated with them.
-		StorageOverflow,
-		// Manipulating an unknown operator
-		UnknownOperator,
-		// Manipulating an unknown request
+		/// Manipulating an unknown operator
+		UnknownProvider,
+		/// Manipulating an unknown request
 		UnknownRequest,
-		// Not the expected operator
-		WrongOperator,
-		// An operator is already registered.
-		OperatorAlreadyRegistered,
-		// Callback cannot be deserialized
+		/// Not the expected operator
+		WrongProvider,
+		/// An operator is already registered.
+		ProviderAlreadyRegistered,
+		/// Callback cannot be deserialized
 		UnknownCallback,
-		// Fee provided does not match minimum required fee
+		/// Fee provided does not match minimum required fee
 		InsufficientFee,
-		// Price does not exist
+		/// Price does not exist
 		PriceDoesNotExist,
-		// Wrong slot to submit
+		/// Wrong socket to submit
 		WrongSocket,
-		// Outlier not determined
+		/// Outlier not determined
 		NotOutlier
 	}
 }
@@ -209,18 +207,19 @@ decl_error! {
 decl_storage! {
 	trait Store for Module<T: Config> as Oracle {
 
-		// A set of all registered Operator
+		// A set of all registered Provider
 		pub Providers get(fn operator): map hasher(blake2_128_concat) <T as frame_system::Config>::AccountId => bool;
+
 		// Price batch from oracle providers
 		pub Prices get(fn asset_price): map hasher(blake2_128_concat) AssetId =>  Option<Vec<Balance>>;
 
-		// Oracles: key as t
+		// Oracles: key as account id, value as oracle socket index
 		pub Oracles get(fn oracle): map hasher(blake2_128_concat)  <T as frame_system::Config>::AccountId  => Option<SocketIndex>;
 
-		// Sockets: key as the oracle slot index, value as the oracle provider
+		// Sockets: key as the oracle socket index, value as the oracle provider
 		pub Sockets get(fn provider_at): map hasher(blake2_128_concat) SocketIndex => <T as frame_system::Config>::AccountId;
 
-		// Slash: key as the oracle slot index, value as the array of slashed accounts
+		// Slash: key as the oracle socket index, value as the array of slashed accounts
 		pub Slashes get(fn slashes_at): map hasher(blake2_128_concat) EraIndex => Vec<<T as frame_system::Config>::AccountId>;
 
 		/// The ideal number of staking participants.
