0b96ff9fcc461858abae317804f3b1e3c43bf335
firke <edvards.sedols@gmail.com>
2022-01-12 14:18:22 +0000

    Runtime bump to 9130.

--- runtime/opportunity/src/lib.rs
+++ runtime/opportunity/src/lib.rs
@@ -124,7 +124,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("opportunity"),
 	impl_name: create_runtime_str!("opportunity10"),
 	authoring_version: 1,
-	spec_version: 9124,
+	spec_version: 9130,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 6,
